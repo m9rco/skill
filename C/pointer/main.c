8729ab0433a877b3e62ad4bf81fd6b2871893efc
pushaowei <pushaowei0727@gmail.com>
2019-01-17 13:21:09 +0800

    fix: update pointer

--- C/pointer/main.c
+++ C/pointer/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
 int main() {
+    int foo = 5;
+    int bar = 10;
+    int *foo_pointer;
+
+    /**
+     * 输出每个变量的地址
+     */
+    printf("&foo %p\n", &foo);
+    printf("&bar %p\n", &bar);
+    printf("&foo_pointer %p\n", &foo_pointer);
+    printf("foo_pointer %p\n", foo_pointer);
+    printf("foo %d\n", foo);
+
+    foo_pointer = &foo;
+    printf("foo_pointer %p\n", foo_pointer);
+    printf("&foo_pointer %p\n", &foo_pointer);
+
+    *foo_pointer = 1;
+
+    printf("foo %d\n", foo);
+
     return 0;
 }
